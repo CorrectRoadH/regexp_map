c658a2e3e06baf7da12523e5599b184d6b85aaa2
CorrectRoadH <a778917369@gmail.com>
2023-08-16 17:45:25 +0800

    add exmaple

--- example/go.mod
+++ example/go.mod
@@ -2,3 +2,6 @@ module example
 
 go 1.20
 
+require (        
+    github.com/CorrectRoadH/regexp_map v0.1.0
+)
--- example/main.go
+++ example/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/CorrectRoadH/regexp_map"
+)
+
+func main() {
+	testMap := regexp_map.NewRegexHashMap[string]()
+	testMap.SetStringKey("https://youtube.com", "youtube")
+	testMap.SetStringKey("https://bilibili.com", "bilibili")
+	testMap.SetRegexpKey("^https?://(?:www\\.)?bilibili\\.com(/[\\w-]+)*/?(\\?[^#]*)?(#.*)?$`", "bilibili")
+
+	result1, ok, key := testMap.Get("https://youtube.com")
+	fmt.Println(result1, ok, key)
+
+	result2, ok, key := testMap.Get("https://bilibili.com")
+	fmt.Println(result2, ok, key)
+
+	result3, ok, _ := testMap.Get("https://www.bilibili.com/video/BV1394y1k7D2/")
+	fmt.Println(result3, ok)
+
+	result4, ok, key := testMap.Get("https://discord.com")
+	fmt.Println(result4, ok, key)
+
+}
