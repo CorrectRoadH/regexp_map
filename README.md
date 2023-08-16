a6c903ef8fbe780bad7f661191707891f3293206
CorrectRoadH <a778917369@gmail.com>
2023-08-16 17:52:05 +0800

    add usage

--- README.md
+++ README.md
@@ -5,5 +5,30 @@ The Regexp Map is a specify Map. Its key can is a string or a a regexp. when the
 
 # Usage 
 ```go  
- 
+package main
+
+import (
+	"fmt"
+
+	"github.com/CorrectRoadH/regexp_map"
+)
+
+func main() {
+	testMap := regexp_map.NewRegexHashMap[string]()
+	testMap.SetStringKey("https://youtube.com", "youtube")
+	testMap.SetStringKey("https://bilibili.com", "bilibili")
+	testMap.SetRegexpKey("^https?://(?:www\\.)?bilibili\\.com(/[\\w-]+)*/?(\\?[^#]*)?(#.*)?$", "bilibili")
+
+	result1, ok, key := testMap.Get("https://youtube.com")
+	fmt.Println(result1, ok, key) // youtube true https://youtube.com
+
+	result2, ok, _ := testMap.Get("https://bilibili.com")
+	fmt.Println(result2, ok) // bilibili true
+
+	result3, ok, key := testMap.Get("https://www.bilibili.com/video/BV1394y1k7D2/")
+	fmt.Println(result3, ok, key) // bilibili true ^https?://(?:www\.)?bilibili\.com(/[\w-]+)*/?(\?[^#]*)?(#.*)?$
+
+	result4, ok, key := testMap.Get("https://discord.com")
+	fmt.Println(result4, ok, key) //  false
+}
 ```
--- example/main.go
+++ example/main.go
@@ -10,18 +10,17 @@ func main() {
 	testMap := regexp_map.NewRegexHashMap[string]()
 	testMap.SetStringKey("https://youtube.com", "youtube")
 	testMap.SetStringKey("https://bilibili.com", "bilibili")
-	testMap.SetRegexpKey("^https?://(?:www\\.)?bilibili\\.com(/[\\w-]+)*/?(\\?[^#]*)?(#.*)?$`", "bilibili")
+	testMap.SetRegexpKey("^https?://(?:www\\.)?bilibili\\.com(/[\\w-]+)*/?(\\?[^#]*)?(#.*)?$", "bilibili")
 
 	result1, ok, key := testMap.Get("https://youtube.com")
-	fmt.Println(result1, ok, key)
+	fmt.Println(result1, ok, key) // youtube true https://youtube.com
 
-	result2, ok, key := testMap.Get("https://bilibili.com")
-	fmt.Println(result2, ok, key)
+	result2, ok, _ := testMap.Get("https://bilibili.com")
+	fmt.Println(result2, ok) // bilibili true
 
-	result3, ok, _ := testMap.Get("https://www.bilibili.com/video/BV1394y1k7D2/")
-	fmt.Println(result3, ok)
+	result3, ok, key := testMap.Get("https://www.bilibili.com/video/BV1394y1k7D2/")
+	fmt.Println(result3, ok, key) // bilibili true ^https?://(?:www\.)?bilibili\.com(/[\w-]+)*/?(\?[^#]*)?(#.*)?$
 
 	result4, ok, key := testMap.Get("https://discord.com")
-	fmt.Println(result4, ok, key)
-
+	fmt.Println(result4, ok, key) //  false
 }
